f47acb6a1bfaa0eeb2d4ccdf2894f8edb6b9d856
redou <test@gmail.com>
2024-12-13 15:52:20 +0100

    Feat : handleDuel

--- src/main/java/dwn/cda/thebot/bot/Bot.java
+++ src/main/java/dwn/cda/thebot/bot/Bot.java
@@ -1,12 +1,17 @@
 package dwn.cda.thebot.bot;
 
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 public class Bot extends ListenerAdapter {
@@ -15,7 +20,12 @@ public class Bot extends ListenerAdapter {
     public void onGuildReady(@NotNull GuildReadyEvent event) {
         guild = event.getGuild();
         guild.updateCommands().addCommands(
-                Commands.slash("hello", "Say Hello")
+                Commands.slash("hello", "Say Hello"),
+                Commands.slash("duel", "Challenge someone to a duel")
+                        .addOptions(
+                                new OptionData(OptionType.USER, "opponent", "The user you want to challenge", true) // Paramètre obligatoire pour l'utilisateur
+                        )
+
         ).queue();
     }
 
@@ -24,6 +34,13 @@ public class Bot extends ListenerAdapter {
         switch (event.getName()) {
             case "hello":
                 event.reply("Hello World").queue();
+                break;
+            case "duel":
+                //  l'utilisateur à défier
+                User opponent = Objects.requireNonNull(event.getOption("opponent")).getAsUser();
+
+                // Message
+                event.reply(String.format("The duel between <@%s> and <@%s> is about to begin!", event.getUser().getId(), opponent.getId())).queue();
                 break;
             default:
                 event.reply("I'm a teapot").setEphemeral(true).queue();
