273ab77196433fbf2fc703dffba8a90980578519
Yanis ADEKALOM <yadekalom@gmail.com>
2024-12-13 12:19:26 +0100

    Feat : Command hello

--- src/main/java/dwn/cda/thebot/bot/Bot.java
+++ src/main/java/dwn/cda/thebot/bot/Bot.java
@@ -1,8 +1,26 @@
 package dwn.cda.thebot.bot;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bot extends ListenerAdapter {
+    private Guild guild;
+    @Override
+    public void onGuildReady(@NotNull GuildReadyEvent event) {
+        guild = event.getGuild();
+        guild.updateCommands().addCommands(
+                Commands.slash("hello", "Say Hello")
+        ).queue();
+    }
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        super.onSlashCommandInteraction(event);
+    }
 }
