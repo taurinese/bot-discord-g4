1c07470981315b387f243b1ec7f0d0a688cbd2ca
Yanis ADEKALOM <yadekalom@gmail.com>
2024-12-13 11:07:48 +0100

    Initial Commit

---
+++ src/main/java/dwn/cda/thebot/ThebotApplication.java
@@ -0,0 +1,13 @@
+package dwn.cda.thebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThebotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThebotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dwn/cda/thebot/ThebotApplicationTests.java
@@ -0,0 +1,13 @@
+package dwn.cda.thebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
