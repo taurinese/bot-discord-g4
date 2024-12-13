ab069fe782c8c1b71e1719b8c1d5a195e95f7367
Yanis ADEKALOM <yadekalom@gmail.com>
2024-12-13 11:15:37 +0100

    Feat : Starting Bot

--- src/main/java/dwn/cda/thebot/ThebotApplication.java
+++ src/main/java/dwn/cda/thebot/ThebotApplication.java
@@ -1,13 +1,47 @@
 package dwn.cda.thebot;
 
+import dwn.cda.thebot.bot.Bot;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class ThebotApplication {
 
+    @Autowired
+    private Bot bot;
+    private static String token;
+
     public static void main(String[] args) {
+        token = args[0];
         SpringApplication.run(ThebotApplication.class, args);
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void starBot() {
+        JDABuilder.createDefault(token)
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
+                .enableIntents(GatewayIntent.GUILD_MEMBERS,
+                        GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                        GatewayIntent.GUILD_MESSAGES,
+                        GatewayIntent.MESSAGE_CONTENT,
+                        GatewayIntent.GUILD_VOICE_STATES
+                )
+                .enableCache(CacheFlag.VOICE_STATE)
+                .setChunkingFilter(ChunkingFilter.ALL)
+                .setBulkDeleteSplittingEnabled(false)
+                .setActivity(Activity.listening("les instructions"))
+                .addEventListeners(bot)
+                .build();
+
     }
 
 }
---
+++ src/main/java/dwn/cda/thebot/bot/Bot.java
@@ -0,0 +1,8 @@
+package dwn.cda.thebot.bot;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bot extends ListenerAdapter {
+}
