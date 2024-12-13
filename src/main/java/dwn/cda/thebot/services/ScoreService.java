d6cf981ca1e5c67ed3e3dd475ba9dca9be750073
taurinese <enzodylan.taurines@gmail.com>
2024-12-13 15:52:10 +0100

    feat: handleScore -> ScoreService permits to get, save or update Score associated with discord user id

--- src/main/java/dwn/cda/thebot/services/ScoreService.java
+++ src/main/java/dwn/cda/thebot/services/ScoreService.java
@@ -1,10 +1,21 @@
 package dwn.cda.thebot.services;
 
+
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ScoreService {
-    public String getScore(String userId) {
-        return "0";
+
+    private final Map<String, Long> scores = new HashMap<String, Long>();
+
+    public Long getScore(String userId) {
+        return scores.computeIfAbsent(userId, id -> 0L);
+    }
+
+    public void saveOrUpdateScore(String userId, Long score){
+        scores.merge(userId, score, Long::sum);
     }
 }
